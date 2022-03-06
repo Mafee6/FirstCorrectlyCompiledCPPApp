09485a477e00a3e8cef0a667ce5488ce54b01c58
Mafee7 <64575804+Mafee6@users.noreply.github.com>
2022-03-06 20:17:55 +0530

    update

--- FirstCorrectlyCompiledCPPApp.cpp
+++ FirstCorrectlyCompiledCPPApp.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 using namespace std;
 
+string thanks () {
+    return "Nice to meet you!";
+}
+
 int main () {
-    cout << "Helo, C++";
+    string op;
+    cout << "Helo, C++\n";
+    cout << "~ Mafee\n";
+    cout << "Any opinions on C++?\n";
+    cin >> op;
+    cout << "Your opinion is:\n" << op << "\n";
+    cout << thanks();
     return 0;
 }
